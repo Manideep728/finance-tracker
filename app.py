c448b4282c820bd98914e52a6a3f3eacd0a88fae
Manideep728 <86245919+Manideep728@users.noreply.github.com>
2026-07-20 23:55:11 -0500

    transaction POST api

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from database import init_db
+from flask import Flask, request
+from database import init_db, add_transaction
+from datetime import date as date_notime
 
 app = Flask(__name__)
 
@@ -12,6 +13,38 @@ def index():
 @app.route("/transactions")
 def transactions():
     return "Transactions will live here"
+
+@app.route("/transactions", methods=['POST'])
+def post_transaction():
+    
+    type_data = {"description":str, "amount":float, "category":str }
+    error_flag = False
+
+    data = request.get_json()
+
+    if len(data) != 4:
+        return "Missing Data", 400
+ 
+    description = data.get("description")
+    amount = data.get("amount")
+    category = data.get("category")
+    try:
+        date = date_notime.fromisoformat(data.get("date"))
+    except ValueError:
+        return "Invalid Date Format", 400
+
+    for key in type_data.keys():
+        if  type_data.get(f"{key}") != type(data.get(f"{key}")):
+            error_flag = True
+    
+    if error_flag: 
+        return "Invalid Input", 400
+    else:
+        add_transaction(description, amount, date, category)
+        return "Successful transaction input", 201
+
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
