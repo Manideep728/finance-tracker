d512c64d5bc31b39d0703937d529596b939fdc96
Manideep728 <86245919+Manideep728@users.noreply.github.com>
2026-07-22 23:35:34 -0500

    index.html done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from database import init_db, add_transaction
 from datetime import date as date_notime
 
@@ -8,7 +8,7 @@ init_db()
 
 @app.route("/")
 def index():
-    return "Finance Tracker is alive"
+    return render_template("index.html")
 
 @app.route("/transactions")
 def transactions():
@@ -22,17 +22,20 @@ def post_transaction():
 
     data = request.get_json()
 
+    
     if len(data) != 4:
         return "Missing Data", 400
  
     description = data.get("description")
     amount = data.get("amount")
     category = data.get("category")
+    # separate date validation cause str in not date obj
     try:
         date = date_notime.fromisoformat(data.get("date"))
     except ValueError:
         return "Invalid Date Format", 400
 
+    # if a field/key in request is invalid, loop catches it as .get returns None, and types don't match
     for key in type_data.keys():
         if  type_data.get(f"{key}") != type(data.get(f"{key}")):
             error_flag = True
